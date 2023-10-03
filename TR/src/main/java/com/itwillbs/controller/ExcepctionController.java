777a1dcffb6fa54d45f3525ecbf7eb972c80458c
jhm <amond@DESKTOP-FAALQIP>
2023-10-03 16:13:22 +0900

    예외 컨트롤러 추가

---
+++ TR/src/main/java/com/itwillbs/controller/ExcepctionController.java
@@ -0,0 +1,20 @@
+package com.itwillbs.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class ExcepctionController {
+	
+	@ExceptionHandler(Exception.class)
+	public void exceptionHandler(Exception e) {
+		log.debug("예외 발생!!" + e );
+		e.printStackTrace();
+	}
+
+}
+
