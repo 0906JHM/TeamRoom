dfdd218b6f68aa0e91581d5c02bc9d167911018f
jhm <amond@DESKTOP-FAALQIP>
2023-10-08 02:38:35 +0900

    조현민 / 거래처 추가 로직 수정하다 실패 .. 다시 원래대로..

--- TR/src/main/java/com/itwillbs/service/ClientService.java
+++ TR/src/main/java/com/itwillbs/service/ClientService.java
@@ -33,6 +33,8 @@ public class ClientService {
 
 	public String  getclientCode(String clientType) {
 		
+		System.out.println("ClientService getClientCode=============" + clientType);
+		
 		return clientDAO.getclientCode(clientType);
 		
 	}
