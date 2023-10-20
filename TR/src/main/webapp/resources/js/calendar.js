1b7ceafb43ed2b9c1c058e9a792886478c3703d6
luretiv <141300899+luretiv@users.noreply.github.com>
2023-10-20 12:45:37 +0900

    kkm 캘린더

--- TR/src/main/webapp/resources/js/calendar.js
+++ TR/src/main/webapp/resources/js/calendar.js
@@ -61,9 +61,9 @@ function initializeCalendar(calendarEvents, currentDate) {
 		    var description = info.event.extendedProps.description;
 		    
 		    if (otherDate !== null) {
-		        var message ='<div style="text-align:left; padding-left:10%;">' + title + '<br> 기간: ' + start + ' ~ ' + otherDate + '<br>' + title + ' 내용: ' + description + '</div>';
+		        var message ='<div style="text-align:left; padding-left:10%;">' + title + '<br> 기간: ' + start + ' ~ ' + otherDate + '<br> 내용: ' + description + '</div>';
 		    } else {
-		        var message ='<div style="text-align:left; padding-left:10%;">' + title + '<br> 기간: ' + start + ' ~ ' + start + '<br>' + title + ' 내용: ' + description + '</div>';
+		        var message ='<div style="text-align:left; padding-left:10%;">' + title + '<br> 기간: ' + start + ' ~ ' + start + '<br> 내용: ' + description + '</div>';
 		    }
 		
 		    Swal.fire({
